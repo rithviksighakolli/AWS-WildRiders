4821aed9b7a66ba096af9c5d49b016fbc50eb6be
Rithvik Sighakolli <rithviksighakolli@gmail.com>
2024-11-14 18:44:33 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_tccweCION', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1l2s3pi8sbc0dgp82t2d3rprhe', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
